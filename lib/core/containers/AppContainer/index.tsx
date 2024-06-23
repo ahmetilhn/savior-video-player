089cb8af08786e515f0c57e19d055c979ea4f987
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-06-23 21:39:45 +0300

    updated animation and css

--- lib/core/containers/AppContainer/index.tsx
+++ lib/core/containers/AppContainer/index.tsx
@@ -1,10 +1,13 @@
 import PanelContainer from "../PanelContainer";
 import VideoContainer from "../VideoContainer";
 import styles from "./index.module.scss";
+type Props = {
+  className: string;
+};
 const AppContainer = () => {
   return (
     <div className={styles["app-container"]}>
-      <PanelContainer />
+      <PanelContainer className={styles["panel-container"]} />
       <VideoContainer />
     </div>
   );
--- lib/core/containers/PanelContainer/index.tsx
+++ lib/core/containers/PanelContainer/index.tsx
@@ -3,9 +3,14 @@ import ProgressBar from "../../components/ProgressBar";
 import SeekBackwardButton from "../../components/SeekBackwardButton";
 import SeekForwardButton from "../../components/SeekForwardButton";
 import styles from "./index.module.scss";
-const PanelContainer = () => {
+type Props = {
+  className: string;
+};
+const PanelContainer: React.FC<React.PropsWithChildren<Props>> = ({
+  className,
+}) => {
   return (
-    <div className={styles["panel-container"]}>
+    <div className={`${styles["panel-container"]} ${className ?? ""}`}>
       <div className={styles.header}></div>
       <div className={styles.middle}>
         <SeekBackwardButton />
