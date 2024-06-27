90c81cb36eb35792eeee568808eb66a491044bcc
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-06-27 22:13:06 +0300

    completed caption feature

---
+++ lib/core/components/Caption/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import useCaption from "../../hooks/useCaption";
+import { store } from "../../store";
+import styles from "./index.module.scss";
+const Caption = () => {
+  const [captionBlock, setCaptionBlock] = useState<
+    CaptionBlockType | undefined
+  >(undefined);
+  const { currentTime, isControlPanelVisible, isPlay } = store(
+    (store) => store
+  );
+  const { getCaptionByTime } = useCaption();
+
+  useEffect(() => {
+    setCaptionBlock(getCaptionByTime(currentTime));
+  }, [currentTime]);
+  return (
+    <>
+      {captionBlock ? (
+        <div
+          style={{ bottom: isControlPanelVisible || !isPlay ? "15%" : "12%" }}
+          className={styles.container}
+        >
+          <span dangerouslySetInnerHTML={{ __html: captionBlock.text }} />
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export default Caption;
--- lib/core/containers/VideoContainer/index.tsx
+++ lib/core/containers/VideoContainer/index.tsx
@@ -1,15 +1,26 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import VideoElement from "../../components/VideoELement";
 import styles from "./index.module.scss";
 import { store } from "../../store";
 import Loader from "../../components/Loader";
+import Caption from "../../components/Caption";
+import useCaption from "../../hooks/useCaption";
 const VideoContainer = () => {
   const videoContainerRef = useRef<HTMLDivElement>(null);
-  const { isControlPanelVisible, isVideoPlayable } = store((store) => store);
+  const { isControlPanelVisible, isVideoPlayable, activeCaption } = store(
+    (store) => store
+  );
+  const { initCaption } = useCaption();
+  useEffect(() => {
+    if (isVideoPlayable && activeCaption) {
+      initCaption();
+    }
+  }, [isVideoPlayable]);
   return (
     <div ref={videoContainerRef} className={styles["video-container"]}>
       {!isVideoPlayable && !isControlPanelVisible && <Loader />}
       <VideoElement />
+      {activeCaption && <Caption />}
     </div>
   );
 };
